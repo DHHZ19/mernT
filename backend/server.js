4863094c9197d3f3b97a98e720220e5e7eb49c44
Diego Hernandez <diegofootball19@gmail.com>
2022-06-10 11:18:14 -0500

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!")
+const express = require('express') 
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use('/api/goals', require('./routes/getRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`server started on port ${port}`))
